ccf22ba61b1269ff6aa97c1dd8e8bae06a0d64ea
Himanshu Gaur <himanshugaur@Himanshus-MacBook-Air.local>
2024-07-13 22:44:24 +0530

    adding home page, tomorrow we will create custom button

---
+++ app/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+const HomePage = () => {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full  h-full items-center bg-green-200 px-4">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[130px] h-[84px]"
+          />
+          <Image
+            source={images.cards}
+            resizeMode="contain"
+            className="max-w-[380px] w-full h-[300px]"
+          />
+          <View className="relative mt-4">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover the endless possibilities with{" "}
+              <Text className="text-secondary-200 font-serif">HivaTech</Text>
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomePage;
