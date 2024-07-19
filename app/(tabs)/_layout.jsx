4914370c95147170c25c4e734d4cc126ea34ea74
Himanshu Gaur <himanshugaur@Himanshus-MacBook-Air.local>
2024-07-19 16:12:16 +0530

    moving the logic of fonts loading to the root layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,4 @@
-import { useFonts } from "expo-font";
-import { SplashScreen, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Image, Text, View } from "react-native";
@@ -20,23 +19,8 @@ const tabBarIcon = (focused, color, size, name) => {
     </View>
   );
 };
-SplashScreen.preventAutoHideAsync();
+
 const AppTabLayout = () => {
-  const [loaded, error] = useFonts({
-    "Poppins-Black": require("../../assets/fonts/Poppins-Black.ttf"),
-    "Poppins-Bold": require("../../assets/fonts/Poppins-Bold.ttf"),
-    "Poppins-ExtraBold": require("../../assets/fonts/Poppins-ExtraBold.ttf"),
-    "Poppins-Light": require("../../assets/fonts/Poppins-Light.ttf"),
-    "Poppins-ExtraLight": require("../../assets/fonts/Poppins-ExtraLight.ttf"),
-    "Poppins-Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-SemiBold": require("../../assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Thin": require("../../assets/fonts/Poppins-Thin.ttf"),
-  });
-  useEffect(() => {
-    if (loaded || error) SplashScreen.hideAsync();
-  }, [loaded, error]);
-  if (!loaded && !error) return null;
   return (
     <>
       <Tabs screenOptions={{ tabBarShowLabel: false }}>
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import { StatusBar } from "react-native";
-
+SplashScreen.preventAutoHideAsync();
 const RootLayout = () => {
+  const [loaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded, error]);
+  if (!loaded) return null;
+  if (!loaded && !error) return null;
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
